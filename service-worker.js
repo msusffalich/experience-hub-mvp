1a3be56d45075247fe50769862d67adace26b58d
msusffalich <msusffalich@gmail.com>
2026-07-27 17:01:30 -0400

    Align user manual with life-area terminology

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "experience-hub-pwa-20260727-life-area-language-714";
+const CACHE_NAME = "experience-hub-pwa-20260727-life-area-manual-715";
 const APP_SHELL = [
   "/icons/vibe-logo.jpg",
   "/icons/vibe-logo.png",
